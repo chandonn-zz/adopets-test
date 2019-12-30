8e25596eb312fcfcf93e526ea9b3be7d6341e271
Alexandre Correa <alexandrecorreadeoliveira@gmail.com>
2019-12-29 21:20:16 -0300

    Improved detailsPage UI

--- src/Views/DetailsPage/index.tsx
+++ src/Views/DetailsPage/index.tsx
@@ -7,12 +7,31 @@ function DetaisPage() {
 	const { id } = useParams();
 	const pet = data.filter(pet => id && pet.id === parseInt(id))[0];
 
+	function transformSize(size: string) {
+		switch(size) {
+			case 'S': return 'Small';
+			case 'M': return 'Medium port';
+			case 'L': return 'Large';
+			case 'XL': return 'Extra large';
+		}
+	}
+
 	return (
-		<div>
-			<span>{pet.name}</span>
-			<span>{pet.breed}</span>
-			<span>{pet.age}</span>
-			<span>{pet.size}</span>
+		<div style={{
+			display: 'flex',
+			minWidth: '100vw',
+			minHeight: '100vh',
+			alignItems: 'center', 
+			flexDirection: 'column',
+			justifyContent: 'center',
+		}}>
+			<div className="details-image-container">
+				<img className="details-image" src={require('../../Resources/Images/dog-1.jpg')} />
+			</div>
+			<p>This is {pet.name}</p>
+			<p>he is a {pet.breed}</p>
+			<p>is {pet.age} years old</p>
+			<p>and is {transformSize(pet.size)} size</p>
 		</div>
 	)
 }
