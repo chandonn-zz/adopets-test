606420910144afc7ab6b1df4f1deb441737a915b
Alexandre Correa <alexandrecorreadeoliveira@gmail.com>
2019-12-29 21:21:05 -0300

    Added auth code to Actions. Added logout and search methods to searchPage

--- src/Actions/index.ts
+++ src/Actions/index.ts
@@ -1,24 +1,57 @@
-export const auth = () => {
-	return false
-}
+import Axios from 'axios';
 
-export const createUser = () => {
-	return false
-}
+const API_KEY = '505763d6-4202-4b05-9efc-93b366939bcf';
+const API_BASE_URL = 'https://test.adopets.app/v1/';
 
 export const loginUser = async (name: string, password: string) => {
-	let posts = await fetch('https://jsonplaceholder.typicode.com/posts');
-	posts = await posts.json();
+	const response = await Axios.post(`${API_BASE_URL}auth/session-request`, { system_api_key: API_KEY });
 
-	console.log(posts)
+	// successful request
+	if (response.data.status === 200) {
+		const token = response.data.data.access_key;
+
+		const register = await Axios.post(
+			`${API_BASE_URL}auth/session-register`,
+			{
+				organization_user: {
+					email: name,
+					password: password
+				}
+			},
+			{
+				headers: { 'Authorization': "Bearer " + token }
+			}
+		)
+		console.log(register)
+
+		// successful session registration
+		if (register.data.status === 200) {
+			const user = {
+				email: name,
+				request_key: register.data.data.access_key,
+			}
+
+			// initiate session and save data to store
+			sessionStorage.setItem('email', name);
+			sessionStorage.setItem('request_key', register.data.data.access_key);
+			
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 
 export const logoutUser = () => {
-	return false
+	sessionStorage.clear();
+
+	return true;
 }
 
-export const searchPets = () => {
-	return false
+export const getPetsFromApi = (code: string, sexKey: string, sizeKey: string, ageKey: string) => {
+	console.log(code, sexKey, sizeKey, ageKey)
+
+	return [];
 }
 
 export const data = [
--- src/Views/SearchPage/index.tsx
+++ src/Views/SearchPage/index.tsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { Layout, Input, Radio, Pagination, Row } from 'antd';
+import { Layout, Input, Radio, Pagination, Row, Button, Icon } from 'antd';
 import ListItem from '../../Components/ListItem';
 
-import { data } from '../../Actions';
+import { logoutUser, data, getPetsFromApi } from '../../Actions';
 
 const { Header, Footer, Sider, Content } = Layout;
 const { Search } = Input;
 
 interface Props {
-
+	onAuthStateChange: () => void;
 }
 
 interface State {
 	searchCode: string;
-	sexKey: 'MALE' | 'FEMALE';
-	sizeKey: 'S' | 'M' | 'L' | 'XL';
-	ageKey: 'BABY' | 'YOUNG' | 'ADULT' | 'SENIOR';
+	sexKey: 'MALE' | 'FEMALE' | '';
+	sizeKey: 'S' | 'M' | 'L' | 'XL' | '';
+	ageKey: 'BABY' | 'YOUNG' | 'ADULT' | 'SENIOR' | '';
+	requestKey: string;
+	results: any;
+	loading: boolean;
 }
 
 class SearchPage extends Component<Props, State> {
@@ -24,13 +27,55 @@ class SearchPage extends Component<Props, State> {
 
 		this.state = {
 			searchCode: '',
-			sexKey: 'MALE',
-			sizeKey: 'S',
-			ageKey: 'BABY'
+			sexKey: '',
+			sizeKey: '',
+			ageKey: '',
+			requestKey: '',
+			results: [],
+			loading: false,
 		}
 	}
 
+	componentDidMount() {
+		const requestKey = sessionStorage.getItem('request_key');
+
+		if (requestKey && requestKey !== '') {
+			this.setState({ requestKey });
+			console.log(requestKey)
+		}
+	}
+
+	finishSession() {
+		const successfulLogout = logoutUser();
+
+		if (successfulLogout) {
+			this.props.onAuthStateChange();
+		}
+	}
+
+	searchPets() {
+		const {
+			sexKey,
+			sizeKey,
+			ageKey,
+			searchCode
+		} = this.state;
+
+		this.setState({ loading: true });
+		
+		const pets = getPetsFromApi(searchCode, sexKey, sizeKey, ageKey);
+
+		if (pets.length) {
+			this.setState({ results: pets }, () => this.setState({ loading: false }));
+		}
+
+		this.setState({ loading: false });
+	}
+
 	render() {
+		const { results, loading } = this.state;
+		console.log(results)
+
 		return (
 			<Layout style={{ minHeight: '100vh' }}>
 				<Sider
@@ -44,10 +89,15 @@ class SearchPage extends Component<Props, State> {
 					}}
 				>
 					<div className="add-margin-vertical">
+						<p style={{ fontWeight: 'bold' }}>
+							How do you want your friend to be like?
+						</p>
 						<p>Sex</p>
 						<Radio.Group
-							onChange={e => this.setState({ sexKey: e.target.value })}
-							defaultValue="MALE"
+							onChange={e => {
+								this.setState({ sexKey: e.target.value }, () => this.searchPets())}
+							}
+							defaultValue=""
 							buttonStyle="solid"
 							size="small"
 						>
@@ -58,8 +108,10 @@ class SearchPage extends Component<Props, State> {
 					<div className="add-margin-vertical">
 						<p>Size</p>
 						<Radio.Group
-							onChange={e => this.setState({ sizeKey: e.target.value })}
-							defaultValue="S"
+							onChange={e => {
+								this.setState({ sizeKey: e.target.value }, () => this.searchPets())}
+							}
+							defaultValue=""
 							buttonStyle="solid"
 							size="small"
 						>
@@ -72,8 +124,10 @@ class SearchPage extends Component<Props, State> {
 					<div className="add-margin-vertical">
 						<p>Age</p>
 						<Radio.Group
-							onChange={e => this.setState({ ageKey: e.target.value })}
-							defaultValue="BABY"
+							onChange={e => {
+								this.setState({ ageKey: e.target.value }, () => this.searchPets())}
+							}
+							defaultValue=""
 							buttonStyle="solid"
 							size="small"
 						>
@@ -86,27 +140,40 @@ class SearchPage extends Component<Props, State> {
 				</Sider>
 				<Layout style={{ marginLeft: 300 }}>
 					<Header style={{ width: '100%', height: 100, background: '#ffffff', position: 'fixed', zIndex: 2, padding: 0 }}>
+						<div id="logout-container">
+							<Button style={{ marginTop: 5 }} onClick={() => this.finishSession()} icon="logout">
+								<span>Logout</span>
+							</Button>
+						</div>
 						<div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center' }}>
 							<Search
 								placeholder="Search pets"
-								onSearch={value => console.log(value)}
+								onSearch={value => this.searchPets()}
 								enterButton
 								style={{ maxWidth: '300px' }}
 								onChange={e => this.setState({ searchCode: e.target.value })}
 							/>
 						</div>
 					</Header>
-					<Content style={{ background: '#ffffff', marginTop: 100 }}>
-						<Row type="flex">
-							{data.map(result => (
-								<ListItem data={result} />
-							))}
-						</Row>
+					{loading ?
+						(<Content style={{ background: '#ffffff', marginTop: 100 }}>
+							<Row type="flex">
+								<Icon type="loading" style={{ marginRight: 10 }}/>
+								<p>Looking for pets...</p>
+							</Row>
+						</Content>) :
+						(<Content style={{ background: '#ffffff', marginTop: 100 }}>
+							<Row type="flex">
+								{this.state.results.map((result: any) => (
+									<div />
+								))}
+							</Row>
 
-						<Row type="flex" justify="center" style={{ margin: '40px 0' }}>
-							<Pagination onChange={(value) => console.log(value)} defaultCurrent={6} total={100} />
-						</Row>
-					</Content>
+							<Row type="flex" justify="center" style={{ margin: '40px 0' }}>
+								<Pagination onChange={(value) => console.log(value)} defaultCurrent={6} total={100} />
+							</Row>
+						</Content>)
+					}
 					<Footer style={{ background: '#ffffff', alignItems: 'center', justifyContent: 'center' }}>
 						<p>Adopets Test App</p>
 						<p>created by Alexandre Correa with ReactJS and AntDesign</p>
